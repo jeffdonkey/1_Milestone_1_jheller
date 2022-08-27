830271832f3d35e863308006b1a4872a3953ecc2
jeffdonkey <8jefa8@gmail.com>
2022-08-27 14:37:23 -0400

    08.27 added img for background

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-// What I need here
-//  -need to receive player name data from HTML
-//  -need to verify alpha only
-//  -need to limit size
-//  -need to update userName variable
+//may not need this file.  keeping as placeholder.  May need later
