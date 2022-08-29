98c68cd104bc01d5dd33a00859beb873f5387f66
jeffdonkey <8jefa8@gmail.com>
2022-08-29 16:06:55 -0400

    first commit of the day

--- noteCollection/grid.js
+++
@@ -1,8 +0,0 @@
-// how to handle the map?
-    // the map is for me. player does not see the map
-
-//BASIC: tie obstacles to the grid location.  Static, not random obstacle.
-
-//start with 3x3 grid and work from there.
-
-//grid object will call in the obstacle object
--- noteCollection/talkitout.js
+++
@@ -1,5 +0,0 @@
-//option to talk it out
-// t-f can obstacle speak? False provides garbage text or other indicator
-// True would give two dialog choices.
-// BASIC: obstacle randomally decides reaction.
-// BETTER: scripted reaction.
