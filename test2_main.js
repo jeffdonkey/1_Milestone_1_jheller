4ec5a9ea4905016702ffddde024fcb3a776fdef4
jeffdonkey <8jefa8@gmail.com>
2022-09-03 14:30:24 -0400

    working on coord class object

--- test2_main.js
+++ test2_main.js
@@ -2,42 +2,76 @@ let playerX = 0
 let playerY = 0
 
 
-function northCoordChange(){
-    console.log("made it");
-    playerY = playerY += 1;
+function xyChange(value1, value2) {
+    playerX = playerX += value1
+    playerY = playerY += value2;
     let playerXString = playerX.toString();
     let playerYString = playerY.toString();
-    let playerCoordString = playerXString.concat(",",playerYString);
-    
-    console.log(playerCoordString)
-} 
+    let playerCoordString = playerXString.concat(",", playerYString);
 
-function southCoordChange(){
-    console.log("made it");
-    playerY = playerY += -1;
-    let playerXString = playerX.toString();
-    let playerYString = playerY.toString();
-    let playerCoordString = playerXString.concat(",",playerYString);
-    
-    console.log(playerCoordString)
-} 
+    // below is logic for changing the background Image.  It does this by 
+    // changing the class name for the body tag 
 
-function westCoordChange(){
-    console.log("made it");
-    playerX = playerX += -1;
-    let playerXString = playerX.toString();
-    let playerYString = playerY.toString();
-    let playerCoordString = playerXString.concat(",",playerYString);
-    
-    console.log(playerCoordString)
-} 
 
-function eastCoordChange(){
-    console.log("made it");
-    playerX = playerX += 1;
-    let playerXString = playerX.toString();
-    let playerYString = playerY.toString();
-    let playerCoordString = playerXString.concat(",",playerYString);
-    
-    console.log(playerCoordString)
-} 
+
+    // let bodyClass = document.getElementsByTagName("body");
+    // bodyClass[0].className = "clownImage";
+
+
+}
+
+
+
+
+class theMap {
+    constructor() { };
+
+    mapLocation(string) {
+        if (string === '0,0') {
+            document.getElementById("topText").innerHTML = "You are in the City";
+            let bodyClass = document.getElementsByTagName("body");
+            bodyClass[0].className = "cityImage";
+        }
+        else if  (string === '0,1') {
+            document.getElementById("topText").innerHTML = "You are in a Field of Clowns";
+            let bodyClass = document.getElementsByTagName("body");
+            bodyClass[0].className = "clownImage";
+        }
+        else if  (string === '0,-1') {
+            document.getElementById("topText").innerHTML = "You are at a Burger Place";
+            let bodyClass = document.getElementsByTagName("body");
+            bodyClass[0].className = "burgerImage";
+        }
+        else if  (string === '1,0') {
+            document.getElementById("topText").innerHTML = "You are at a Park";
+            let bodyClass = document.getElementsByTagName("body");
+            bodyClass[0].className = "parkImage";
+        }
+        else if  (string === '1,1') {
+            document.getElementById("topText").innerHTML = "You are at a Pizzeria";
+            let bodyClass = document.getElementsByTagName("body");
+            bodyClass[0].className = "pizzaImage";
+        }
+        else if  (string === '1,-1') {
+            document.getElementById("topText").innerHTML = "You are at a Refugee Camp";
+            let bodyClass = document.getElementsByTagName("body");
+            bodyClass[0].className = "campImage";
+        }
+        else if  (string === '-1,0') {
+            document.getElementById("topText").innerHTML = "You are at a Club";
+            let bodyClass = document.getElementsByTagName("body");
+            bodyClass[0].className = "clubImage";
+        }
+        else if  (string === '-1,1') {
+            document.getElementById("topText").innerHTML = "You are at the Temple of Capitalism";
+            let bodyClass = document.getElementsByTagName("body");
+            bodyClass[0].className = "templeImage";
+        }
+        else (string === '-1,-1') {
+            document.getElementById("topText").innerHTML = "You are at the Worst Place on Earth";
+            let bodyClass = document.getElementsByTagName("body");
+            bodyClass[0].className = "tparkImage";
+        }
+
+    }
+}
